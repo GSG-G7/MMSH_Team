6937fe659ffba59c03c3d644a4d2d141fb2e002f
mossa-Sammer <mossa708496@gmail.com>
2019-07-04 10:24:35 +0300

    team social media and navbar fixed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 const navBar = document.querySelector('#main-nav');
 const showcase = document.querySelector('.showcase');
-
-
+const menu = document.querySelector('.menu');
+const navBarList = document.querySelector('.navbarList');
+const logo = document.querySelector('.logo');
 window.addEventListener('scroll',(e)=>{
-console.log(showcase.offsetTop);
-if(window.scrollY>showcase.offset().bottom)
-{
+
+    console.log(showcase.offsetTop);
+
+if(window.scrollY>500){
+
     navBar.classList.add("nav-colored");
     navBar.classList.remove("nav-transparent");
 
 }
 
 else{
-
     navBar.classList.add("nav-transparent");
-        navBar.classList.remove("nav-colored");
+    navBar.classList.remove("nav-colored");
 }
+});
+
+menu.addEventListener('click',()=>{
+
+    if(navBarList.style.display=='none'){
+        navBarList.style.display="block";
+        logo.style.display="none";
+    }
+    else navBarList.style.display="none";
 
 });
 
